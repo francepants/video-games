5643802a5a0b51539465f7fa2fefa1f6bd82e2a9
francepants <franceline90@gmail.com>
2020-10-11 18:52:16 -0500

    added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+VideoGame.create([
+    {
+        game_name: "Mario Kart 8",
+        game_genre: "Kart Racing",
+        game_rating: "E for Everyone",
+        game_platform: "Nintendo Switch",
+        year_released: 2017,
+        description: "Mario Kart 8 is a 2014 kart racing game developed and published by Nintendo for its Wii U home video game console in May 2014. It retains Mario Kart series game mechanics, where players control Mario franchise characters in kart racing, collecting a variety of items to hinder opponents or gain advantages in the race."},
+    {
+        game_name: "Cyberpunk 2077",
+        game_genre: "Action role-playing game, First-person shooter, Open world",
+        game_rating: "M for Mature",
+        game_platform: "PlayStation 5, Xbox Series X and Series S, Google Stadia, Xbox One, Microsoft Windows",
+        year_released: 2020,
+        description: "Cyberpunk 2077 is an upcoming action role-playing video game developed and published by CD Projekt. It is scheduled to be released for PlayStation 4, Windows, and Xbox One in November 2020, Stadia by the end of the year, and PlayStation 5 and Xbox Series X/S in 2021."
+    },
+    {
+        game_name: "Pokemon Snap",
+        game_genre: "Rail shooter",
+        game_rating: "E for Everyone",
+        game_platform: "Nintendo 64, Wii, Wii U",
+        year_released: 1999,
+        description: "Pokémon Snap is a first-person simulation video game with rail shooter style gameplay mechanics co-developed by HAL Laboratory and Pax Softnica and published by Nintendo for the Nintendo 64."
+    }
+])
