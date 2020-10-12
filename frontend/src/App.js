55860c5004f3b34cfa1f05a0de2ccbcadecc1133
francepants <franceline90@gmail.com>
2020-10-12 12:50:56 -0500

    checking that fetch works with users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  componentDidMount(){
+    fetch("http://localhost:3001/users")
+    .then(resp => resp.json())
+    .then(console.log)
+  }
+
   render() {
     return (
       <div className="App">
