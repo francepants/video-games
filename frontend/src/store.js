2b43937e56e768bdf30bb8e4ebd98f68e57e8c9e
francepants <franceline90@gmail.com>
2020-10-14 10:24:52 -0500

    added video games reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import usersReducer from './reducers/usersReducer'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginFormReducer'
+import videoGames from './reducers/videoGamesReducer'
 import thunk from 'redux-thunk'
 // import rootReducer from './reducers/rootReducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
@@ -10,7 +11,8 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 const reducer = combineReducers({
     user: usersReducer, 
     currentUser,
-    loginForm
+    loginForm,
+    videoGames
   })
   
   const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
