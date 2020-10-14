d302e1710120639aad83ec23939dc0a627df0f15
francepants <franceline90@gmail.com>
2020-10-14 10:27:39 -0500

    main cont added to display video games

---
+++ frontend/src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import VideoGames from '../components/VideoGames'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+            <VideoGames />
+        </div>
+    )
+}
+
+export default MainContainer
