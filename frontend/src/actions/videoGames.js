e6d4d2556856d616f34a2eed45cb5626cb4b1777
francepants <franceline90@gmail.com>
2020-10-14 10:26:11 -0500

    video games action created to set and get them

---
+++ frontend/src/actions/videoGames.js
@@ -0,0 +1,34 @@
+// sync
+
+export const setVideoGames = videoGames => {
+    return {
+        type: "SET_VIDEO_GAMES",
+        videoGames
+    }
+} 
+
+//async
+export const getVideoGames = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/video_games", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+                console.log(resp.data)
+                dispatch(setVideoGames(resp.data))
+            }
+        }
+            
+        )
+        .catch(console.log)
+
+    }
+}
