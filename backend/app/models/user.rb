a75964f7dc5716cccb53edf6448b8c7c24100365
francepants <franceline90@gmail.com>
2020-10-14 16:36:05 -0500

    added validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :comments
     has_many :video_games, through: :comments
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
--- backend/app/models/video_game.rb
+++ backend/app/models/video_game.rb
@@ -1,4 +1,14 @@
 class VideoGame < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
+
+    validates :game_name, presence: true, uniqueness: true
+    validates :game_genre, presence: true
+    validates :game_rating, presence: true
+    validates :game_platform, presence: true
+    validates :description, presence: true
+    validates :year_released, presence: true
+
 end
+
+
