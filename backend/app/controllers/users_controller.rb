787e9699815129621ecdd46ae66f642f45038f6a
francepants <franceline90@gmail.com>
2020-10-15 01:37:26 -0500

    backend params edited, and create

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -20,11 +20,15 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      # binding.pry
+      session[:user_id] = @user.id
+      render json: @user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
@@ -50,6 +54,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
