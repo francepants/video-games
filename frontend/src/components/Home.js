1b200c547b045b56d9acafeca1f63c25e463332c
francepants <franceline90@gmail.com>
2020-10-15 01:35:33 -0500

    Home set up with signup and login links

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Login from './LoginForm'
+// import Signup from './Signup'
+// import Logout from './Logout'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+    return (
+            <div>
+                <h1>Welcome!</h1>
+                <h4>Please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h4>
+            </div>
+    );
+}
+
+export default Home;
