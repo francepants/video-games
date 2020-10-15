6cb033d02be6d04ee0b13e3ac9d9d5e561fbd73a
francepants <franceline90@gmail.com>
2020-10-15 01:34:20 -0500

    navbar setup

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import Login from './LoginForm'
+// import Login from './LoginForm'
 import Logout from './Logout'
+import { connect } from 'react-redux'
+import { NavLink, Link } from 'react-router-dom'
 
+//functional component
 
-const NavBar = ({currentUser}) => {
+const NavBar = ({currentUser, loggedIn}) => {
     return (
-        <div>
-            <ul>
-                {currentUser ? `Welcome ${currentUser.attributes.username}!` : ""}
-                {currentUser ? <Logout/> : <Login/>}
-            </ul>
-        </div>
+        <nav className="nav-wrapper purple darken-4">
+            <div className="container">
+                <Link className="brand-logo left" to="/">Video Games</Link>
+                <ul className="right">
+                    <li> <NavLink exact to="/videoGames">My Video Games</NavLink></li>
+                    <li> <NavLink exact to="/videoGames/new">New Video Game</NavLink></li>
+                    <li> { loggedIn ? <Logout/> : null }</li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
 const mapStateToProps = ({currentUser}) => {
     return {
-        currentUser
+        currentUser,
+        loggedIn: !!currentUser
     }
 }
 
 export default connect(mapStateToProps)(NavBar)
+
+// {currentUser ? `Welcome ${currentUser.attributes.username}!` : ""}
+                // {/* {currentUser ? <Logout/> : <Login/>} */}
