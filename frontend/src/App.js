33fc74acb1a8ea18957dbfdc65e9145915492ecc
francepants <franceline90@gmail.com>
2020-10-15 01:36:12 -0500

    working on app view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Login from './components/LoginForm'
+// import Logout from './components/Logout'
+import Signup from './components/Signup'
+import VideoGames from './components/VideoGames'
+import NewVideoGameForm from './components/NewVideoGameForm'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
-import MainContainer from './containers/MainContainer'
+import { Route, Switch, withRouter } from 'react-router-dom'
+// import MainContainer from './containers/MainContainer'
+
 
 class App extends Component {
   componentDidMount(){
@@ -10,15 +18,26 @@ class App extends Component {
   }
 
   render() {
+    const { loggedIn } = this.props
     return (
-      <div className="App">
-        <NavBar />
-        <MainContainer />
-      </div>
+        <div className="App">
+          { loggedIn ? <NavBar /> : <Home />}
+          <Switch>
+              {/* <Route exact path='/' render={(props) => loggedIn ? <VideoGames/> : <Home/>}/> */}
+              <Route exact path='/signup' component={Signup}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/videoGames' component={VideoGames}/>
+              <Route exact path='/videoGames/new' component={NewVideoGameForm}/>
+            </Switch>
+        </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
 
-
-export default connect(null, { getCurrentUser })(App)
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App))
