377d5400e461d04a9d99ce688cab52167d2939a2
francepants <franceline90@gmail.com>
2020-10-15 20:31:23 -0500

    added belongs to optional true relationship

--- backend/app/models/video_game.rb
+++ backend/app/models/video_game.rb
@@ -1,6 +1,7 @@
 class VideoGame < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
+    belongs_to :user, optional: true
 
     validates :game_name, presence: true, uniqueness: true
     validates :game_genre, presence: true
