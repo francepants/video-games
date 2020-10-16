f2afe1a7d81fbfa981c1b2ea7cf21306f0f68391
francepants <franceline90@gmail.com>
2020-10-15 22:05:12 -0500

    action for new video game form

--- frontend/src/actions/newVideoGameForm.js
+++ frontend/src/actions/newVideoGameForm.js
@@ -1,9 +1,10 @@
-//works with handle change in component > NewVideoGameForm
+ //works with handle change in component > NewVideoGameForm
 
 export const updateNewVideoGameForm = (name, value) => {
-    console.log(name, value)
+    const formData = { name, value }
+    console.log("action formData:", formData)
     return {
         type: "UPDATE_NEW_VIDEO_GAME_FORM",
-        formData: { name, value }
+        formData
     }
 }
