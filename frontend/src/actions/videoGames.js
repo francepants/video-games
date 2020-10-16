80c3bd912537caae0730833c194d88c3c8f371a7
francepants <franceline90@gmail.com>
2020-10-15 22:05:33 -0500

    add video game and create

--- frontend/src/actions/videoGames.js
+++ frontend/src/actions/videoGames.js
@@ -8,9 +8,16 @@ export const setVideoGames = videoGames => {
 } 
 export const clearVideoGames = () => {
     return {
-        type: "CLEAR_VIDEO_GAMES",
+        type: "CLEAR_VIDEO_GAMES"
     }
 } 
+
+export const addVideoGame = (videoGame) => {
+    return {
+        type: "ADD_VIDEO_GAME",
+        videoGame
+    } 
+}
 
 //async
 export const getVideoGames = () => {
@@ -35,5 +42,32 @@ export const getVideoGames = () => {
         )
         .catch(console.log)
 
+    }
+}
+
+export const createVideoGame = (videoGameData) => {
+    return dispatch => {
+        const updatedRailsData = {
+            video_game: {
+                game_name: videoGameData.gameName,
+                game_genre: videoGameData.gameGenre,
+                game_rating: videoGameData.gameRating,
+                game_platform: videoGameData.gamePlatform,
+                description: videoGameData.description,
+                year_released: videoGameData.yearReleased,
+                user_id: videoGameData.userId
+            }
+        }
+        return fetch("http://localhost:3001/video_games/", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedRailsData) 
+        })
+        .then(resp => resp.json())
+        .then(console.log)
+        .catch(console.log)
     }
 }
