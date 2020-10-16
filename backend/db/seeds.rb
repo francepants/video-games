a20ea181f01c10a08adb383c24093ef1f071139a
francepants <franceline90@gmail.com>
2020-10-15 20:30:59 -0500

    reorganized seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,12 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create([
-#     {username: "sunshine",
-#     email: "sunshine@email.com",
-#     password: "summertime"}
-# ])
+user = User.create([
+    {username: "sunshine",
+    email: "sunshine@email.com",
+    password: "summertime"}
+])
 
+sunshine = User.first 
+
+sunshine_game = sunshine.video_games.create(game_name: "Pokemon Snap",
+    game_genre: "Rail shooter",
+    game_rating: "E for Everyone",
+    game_platform: "Nintendo 64, Wii, Wii U",
+    year_released: 1999,
+    description: "Pokémon Snap is a first-person simulation video game with rail shooter style gameplay mechanics co-developed by HAL Laboratory and Pax Softnica and published by Nintendo for the Nintendo 64.")
+
+sunshine_game = sunshine.video_games.create(game_name: "Cyberpunk 2077",
+    game_genre: "Action role-playing game, First-person shooter, Open world",
+    game_rating: "M for Mature",
+    game_platform: "PlayStation 5, Xbox Series X and Series S, Google Stadia, Xbox One, Microsoft Windows",
+    year_released: 2020,
+    description: "Cyberpunk 2077 is an upcoming action role-playing video game developed and published by CD Projekt. It is scheduled to be released for PlayStation 4, Windows, and Xbox One in November 2020, Stadia by the end of the year, and PlayStation 5 and Xbox Series X/S in 2021.")
 
 # VideoGame.create([
 #     {
@@ -56,18 +71,3 @@
 #     description: "Cyberpunk 2077 is an upcoming action role-playing video game developed and published by CD Projekt. It is scheduled to be released for PlayStation 4, Windows, and Xbox One in November 2020, Stadia by the end of the year, and PlayStation 5 and Xbox Series X/S in 2021."
 # )
 
-# sunshine = User.first 
-
-# sunshine_game = sunshine.video_games.create(game_name: "Pokemon Snap",
-#     game_genre: "Rail shooter",
-#     game_rating: "E for Everyone",
-#     game_platform: "Nintendo 64, Wii, Wii U",
-#     year_released: 1999,
-#     description: "Pokémon Snap is a first-person simulation video game with rail shooter style gameplay mechanics co-developed by HAL Laboratory and Pax Softnica and published by Nintendo for the Nintendo 64.")
-
-# sunshine_game = sunshine.video_games.create(game_name: "Cyberpunk 2077",
-#     game_genre: "Action role-playing game, First-person shooter, Open world",
-#     game_rating: "M for Mature",
-#     game_platform: "PlayStation 5, Xbox Series X and Series S, Google Stadia, Xbox One, Microsoft Windows",
-#     year_released: 2020,
-#     description: "Cyberpunk 2077 is an upcoming action role-playing video game developed and published by CD Projekt. It is scheduled to be released for PlayStation 4, Windows, and Xbox One in November 2020, Stadia by the end of the year, and PlayStation 5 and Xbox Series X/S in 2021.")
