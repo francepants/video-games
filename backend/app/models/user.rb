287154ad887e247236c4fedbedf8638525661c25
francepants <franceline90@gmail.com>
2020-10-17 10:09:25 -0500

    edited relationships

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :comments
-    has_many :video_games, through: :comments
+    has_many :video_games
+    
 
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
--- backend/app/models/video_game.rb
+++ backend/app/models/video_game.rb
@@ -1,9 +1,11 @@
 class VideoGame < ApplicationRecord
     has_many :comments
     has_many :users, through: :comments
-    belongs_to :user, optional: true
+    has_many :users, through: :user_video_game
+    belongs_to :user
+    # , optional: true
 
-    validates :game_name, presence: true, uniqueness: true
+    validates :game_name, presence: true
     validates :game_genre, presence: true
     validates :game_rating, presence: true
     validates :game_platform, presence: true
