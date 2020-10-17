93215fc17299cafe90c8298145cd5adc6efe01d7
francepants <franceline90@gmail.com>
2020-10-17 10:14:22 -0500

    added add video game and initial state

--- frontend/src/reducers/videoGamesReducer.js
+++ frontend/src/reducers/videoGamesReducer.js
@@ -1,14 +1,18 @@
-// const initialState = {
-//     loading: true,
-//     videoGames: []
-// }
+const initialState = {
+    loading: true,
+    videoGames: []
+}
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
     switch(action.type){
         case "SET_VIDEO_GAMES":
             return action.videoGames
+        case "ADD_VIDEO_GAME":
+            // debugger
+            // so it does show the new game 
+            return state.concat(action.videoGame.data)
         case "CLEAR_VIDEO_GAMES":
-            return []
+            return initialState
         default:
             return state
     }
