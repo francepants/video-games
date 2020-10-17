e4331c3423888130b0b11405d310c42b70e69a91
francepants <franceline90@gmail.com>
2020-10-17 12:37:43 -0500

    edited card - displays VG data and edit link (working on that)

--- frontend/src/components/VideoGameCard.js
+++ frontend/src/components/VideoGameCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const VideoGameCard = ({videoGame}) => {
+const VideoGameCard = ({ videoGame }) => {
     return (
-        <div className="card">
-            
-            <p className="card-title">{videoGame.attributes.game_name}</p>
-        </div>
-    );
-}
+        videoGame ? 
+            <div className="card">
+                <h3>{videoGame.attributes.game_name}</h3>
+                <p>{videoGame.attributes.year_released}</p>
+                <p>{videoGame.attributes.game_genre}</p>
+                <p>{videoGame.attributes.game_rating}</p>
+                <p>{videoGame.attributes.game_platform}</p>
+                <p>{videoGame.attributes.description}</p>
+                
+                <Link to={`/videoGame/${videoGame.id}/edit`}>Edit Game</Link>
+            </div> :
+            <p>lalala</p>
+    )
+} 
 
 export default VideoGameCard
