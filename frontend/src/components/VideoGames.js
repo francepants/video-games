605afcb1f6a48a77c982a78638351c428c8bb023
francepants <franceline90@gmail.com>
2020-10-17 10:13:15 -0500

    added links to video game itself, but not in use yet

--- frontend/src/components/VideoGames.js
+++ frontend/src/components/VideoGames.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import VideoGameCard from './VideoGameCard';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 
 
 const VideoGames = (props) => {
+    // links to VG itself
+//     const videoGameCards = props.videoGames.length > 0 ?
+//     props.videoGames.map(vg => (<p key={vg.id}><Link to={`/videoGames/${vg.id}`}>{vg.attributes.game_name}</Link></p>)) : null
+
+//   return videoGameCards
+
     const videoGameCards = props.videoGames.map(v => <VideoGameCard videoGame={v} key={v.id}/>)
     return (
         videoGameCards.length > 0 ? videoGameCards : null
-            // {videoGameCards}
-       
+            // {videoGameCards}       
     );
 }
 
