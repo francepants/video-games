bc2e59d09388de4624edb06bffb8f26763d56e02
francepants <franceline90@gmail.com>
2020-10-18 18:55:42 -0500

    added preset edit form data

--- frontend/src/actions/newVideoGameForm.js
+++ frontend/src/actions/newVideoGameForm.js
@@ -13,3 +13,18 @@ export const resetNewVideoGameForm = () => {
         type: "RESET_NEW_VIDEO_GAME_FORM"
     }
 }
+
+export const presetEditFormData = videoGame => {
+    const vgFormData = {
+        gameName: videoGame.attributes.game_name,
+        gameGenre: videoGame.attributes.game_genre,
+        gameRating: videoGame.attributes.game_rating,
+        gamePlatform: videoGame.attributes.game_platform,
+        description: videoGame.attributes.description,
+        yearReleased: videoGame.attributes.year_released
+    }
+    return {
+        type: "PRESET_EDIT_FORM_DATA",
+        vgFormData
+    }
+}
