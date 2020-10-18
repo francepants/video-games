1e9ca259fd4a5548ba8fc722bd8d13c5a4b2e5c6
francepants <franceline90@gmail.com>
2020-10-18 18:52:34 -0500

    commented out

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import VideoGames from '../components/VideoGames'
+// import React from 'react'
+// import VideoGames from '../components/VideoGames'
 
-const MainContainer = () => {
-    return (
-        <div className="MainContainer">
-            <VideoGames />
-        </div>
-    )
-}
+// const MainContainer = () => {
+//     return (
+//         <div className="MainContainer">
+//             <VideoGames />
+//         </div>
+//     )
+// }
 
-export default MainContainer
+// export default MainContainer
