5fb50fc644e65f876d073c8c1b282bd8d10f9d95
francepants <franceline90@gmail.com>
2020-10-18 22:29:22 -0500

    deleted unused code

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-// import usersReducer from './reducers/usersReducer'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginFormReducer'
 import videoGames from './reducers/videoGamesReducer'
 import signupForm from './reducers/signupFormReducer'
 import newVideoGameForm from './reducers/newVideoGameFormReducer'
 import thunk from 'redux-thunk'
-// import rootReducer from './reducers/rootReducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 
 const reducer = combineReducers({
-    // user: usersReducer, 
     currentUser,
     loginForm,
     videoGames,
