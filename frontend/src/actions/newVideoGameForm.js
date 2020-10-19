db3a882f66d1e9b0a7850def0b2e0dd641695ad4
francepants <franceline90@gmail.com>
2020-10-18 22:30:38 -0500

    commented out console.log

--- frontend/src/actions/newVideoGameForm.js
+++ frontend/src/actions/newVideoGameForm.js
@@ -2,7 +2,7 @@
 
 export const updateNewVideoGameForm = (name, value) => {
     const formData = { name, value }
-    console.log("action formData:", formData)
+    // console.log("action formData:", formData)
     return {
         type: "UPDATE_NEW_VIDEO_GAME_FORM",
         formData
