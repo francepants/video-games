7fff2ec80ec180043a7d63d586ce343c953be2ec
francepants <franceline90@gmail.com>
2020-10-18 22:19:52 -0500

    delete working

--- frontend/src/reducers/videoGamesReducer.js
+++ frontend/src/reducers/videoGamesReducer.js
@@ -8,12 +8,12 @@ export default (state = initialState, action) => {
         case "SET_VIDEO_GAMES":
             return action.videoGames
         case "ADD_VIDEO_GAME":
-            // debugger
-            // so it does show the new game 
             return state.concat(action.videoGame.data)
         case "UPDATE_VIDEO_GAME":
             // console.log("update video game action is: ", action)
             return state.map(videoGame => videoGame.id === action.videoGame.id ? action.videoGame : videoGame)
+        case "DELETE_VIDEO_GAME":
+            return state.filter(videoGame => videoGame.id === action.videoGameId ? false : true)
         case "CLEAR_VIDEO_GAMES":
             return initialState
         default:
