725fbe7d8232b3f8d371de37ed8bc9abd08c9b3e
francepants <franceline90@gmail.com>
2020-10-18 22:22:18 -0500

    removed unused code

--- frontend/src/components/VideoGames.js
+++ frontend/src/components/VideoGames.js
@@ -8,19 +8,12 @@ import { Link } from 'react-router-dom'
 const VideoGames = (props) => {
     // debugger
     // props.videoGames.length shows all video games in the array
-
+    
     // links to VG itself 
-    const videoGameCards = props.videoGames.length > 0 ? 
-    // bug gets triggered here: TypeError: videoGames.find is not a function
+    const videoGameCards = props.videoGames.length > 0 ?     
     props.videoGames.map(vg => (<h4 key={vg.id}><Link to={`/videoGames/${vg.id}`}>{vg.attributes.game_name}</Link></h4>)) : null
 
     return videoGameCards
-
-    // const videoGameCards = props.videoGames.map(v => <VideoGameCard videoGame={v} key={v.id}/>)
-    // return (
-    //     videoGameCards.length > 0 ? videoGameCards : null
-    //         // {videoGameCards}       
-    // );
 }
 
 // state of the store
