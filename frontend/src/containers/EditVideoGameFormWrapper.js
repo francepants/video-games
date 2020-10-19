78dee007d593f16bed0164f8e69ab84030567bf5
francepants <franceline90@gmail.com>
2020-10-18 20:30:28 -0500

    component did update, will unmount; reset form

--- frontend/src/containers/EditVideoGameFormWrapper.js
+++ frontend/src/containers/EditVideoGameFormWrapper.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import NewVideoGameForm from '../components/NewVideoGameForm'
 import { updateVideoGame } from '../actions/videoGames'
-import { presetEditFormData } from '../actions/newVideoGameForm'
+import { presetEditFormData, resetNewVideoGameForm } from '../actions/newVideoGameForm'
 // import { getVideoGames } from '../actions/videoGames'
 import { connect } from 'react-redux'
-
 
 
 class EditVideoGameFormWrapper extends React.Component {
@@ -12,23 +11,33 @@ class EditVideoGameFormWrapper extends React.Component {
         this.props.videoGame && this.props.presetEditFormData(this.props.videoGame)
     }
 
-    handleSubmit = (formData, userId) => {
-        const {updateVideoGame, videoGame, history} = this.props
+    componentDidUpdate(prevProps) {
+        this.props.videoGame && !prevProps.videoGame && this.props.presetEditFormData(this.props.videoGame)
+    }
+
+    componentWillUnmount() {
+        this.props.resetNewVideoGameForm()
+    }
+
+    handleSubmit = (formData) => {
+        const {updateVideoGame, videoGame, history} = this.props 
       
         updateVideoGame({
             ...formData,
-            videoGameId: videoGame.id,
-            userId
+            videoGameId: videoGame.id
         }, history)
         // getVideoGames()
     }
 
     render() {
         const {history, handleSubmit} = this.props
-        return <NewVideoGameForm editMode handleSubmit={this.handleSubmit}/>
+        return <>
+                <NewVideoGameForm editMode handleSubmit={this.handleSubmit}/>
+                <button onClick={this.deleteVideoGame}>Delete</button>
+            </>
     }
 
 }
 
 
-export default connect(null, { updateVideoGame, presetEditFormData })(EditVideoGameFormWrapper)
+export default connect(null, { updateVideoGame, presetEditFormData, resetNewVideoGameForm })(EditVideoGameFormWrapper)
