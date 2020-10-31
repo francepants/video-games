5420bd8dac1d6cbc524c530296cd40b7ae2539a1
francepants <franceline90@gmail.com>
2020-10-31 10:24:03 -0500

    put emoji in a span

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = ({currentUser, loggedIn}) => {
     return (
         <nav className="nav-wrapper grey darken-4">
             <div className="collapse navbar-collapse">
-                <Link className="logo left" to='/loggedInWelcome'>👾 Video Games</Link>
+                <Link className="logo left" to='/loggedInWelcome'><span role="img" aria-label="">👾</span> Video Games</Link>
                 <ul className="collapse navbar-collapse right">
                     <li className="nav-item"> <NavLink exact to="/videoGames">My Video Games</NavLink></li>
                     <li className="nav-item"> <NavLink exact to="/videoGames/new">New Video Game</NavLink></li>
